0a2b05c118519d0f10fafede59ea01075d15589c
Steve Cliff <steve.cliff@atos.net>
2021-06-29 11:30:28 +0100

    Basic Consul HA working (without security)

--- 02-consul.sh
+++ 02-consul.sh
@@ -2,37 +2,64 @@
 
 # Params
 NIC="eth0"
-IP="192.168.1.2"
+IPPREFIX="192.168.1."
 PREFIX=consul
+SRVMAX=3
 
 # Re-create network
 docker network rm vault-consul
 docker network create \
   --attachable \
   --driver=bridge \
-  --subnet=192.168.1.0/24 \
-  --gateway=192.168.1.1 vault-consul
+  --subnet=${IPPREFIX}0/24 \
+  --gateway=${IPPREFIX}254 vault-consul
 
-# Run initial lead agent
-docker run -d \
-  --name=${PREFIX}1 -h ${PREFIX}1 \
-  --restart=unless-stopped \
-  --network vault-consul \
-  -e CONSUL_BIND_INTERFACE=${NIC} \
-  -v /data/consul/1:/consul/data \
-  --ip ${IP} \
-  -p 8500:8500 \
-  consul
+# Build server list
+SRVLST=""
+for srv in $(eval echo "{1..$SRVMAX}")
+do 
+  SRVLST+="\"$IPPREFIX${srv}\""
+  if [ $srv != ${SRVMAX} ]; then
+    SRVLST+=", "
+  fi
+done
 
-# Run other agents using client mode
-for agent in {2..5}; do 
-  docker run -d \
-    --name=${PREFIX}${agent} -h ${PREFIX}${agent} \
-    --restart=unless-stopped \
-    --network vault-consul \
-    -e CONSUL_BIND_INTERFACE=${NIC} \
-    -e 'CONSUL_LOCAL_CONFIG={"leave_on_terminate": true}' \
-    -v /data/consul/$agent:/consul/data \
-    consul \
-    agent -join=${IP}
+# Write out fresh server JSON
+cat <<EOF > server.json
+{
+  "datacenter": "dcvault",
+  "client_addr": "0.0.0.0",
+  "leave_on_terminate": true, 
+  "server": true, 
+  "ui": true,
+  "bootstrap_expect": ${SRVMAX},
+  "acl_enforce_version_8": false, 
+  "retry_join": [ ${SRVLST} ]
+}
+EOF
+
+# Run Consul servers
+# NB. ACL disabled for initial test - needs enabling later
+#
+for srv in $(eval echo "{1..$SRVMAX}")
+do
+  # Copy latest config
+  mkdir -p /data/consul/${srv}/config
+  cp server.json /data/consul/${srv}/config/server.json
+  # Set up basic run command
+  DRUN="--name=consul${srv} -h consul${srv} --restart=unless-stopped --network vault-consul -e CONSUL_BIND_INTERFACE=${NIC} -v /data/consul/${srv}/data:/consul/data -v /data/consul/${srv}/config:/consul/config -v /data/consul/${srv}/logs:/consul/logs --ip ${IPPREFIX}${srv}"
+  # Add options
+  OPT=""
+  case "$srv" in
+    1)
+      DRUN+=" -p 8500:8500"
+      ;;
+    *)
+      ;;
+  esac
+  # Finish command
+  DRUN+=" consul agent --server ${OPT}"
+    # Start server
+  echo Starting server ${srv} ...
+  docker run -d ${DRUN}
 done
